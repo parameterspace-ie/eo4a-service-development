d5d00389fca56282bfddb574f1b2745a65d31d06
Ana Juracic <ana.juracic@parameterspace.ie>
2017-08-22 14:05:23 +0100

    Example OGR service, merging multile shapefiles into a single file. @issue: 4589 @time: 4.0 @progress: 20 @activity: Development Branch: develop *Changed Files:* M	.gitignore A	shapefiles/__init__.py A	shapefiles/merge_shapefiles.sh A	shapefiles/wps_shapefiles.py M	wps_definition.py

--- wps_definition.py
+++ wps_definition.py
@@ -4,6 +4,8 @@ WPS processes for example services for the EO4Atlantic platform
 
 from sentinel.sentinel2 import Sentinel2Rgb
 
+from shapefiles.wps_shapefiles import MergeShapefiles
+
         
         
         
